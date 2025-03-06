5c91907a1945ffcecc8cb077d789ad82448e4cf4
Rgautier35 <160230909+Rgautier35@users.noreply.github.com>
2025-03-06 10:12:37 +0100

    Process inputs, keep window opened and handle window resize

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 #include <glad/glad.h>
 #include <glfw3.h>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
 
 int main() 
 {
@@ -19,6 +30,31 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	while (!glfwWindowShouldClose(window))
+	{
+		//Input
+		processInput(window);
+
+		//Rendering commands
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Check and call events and swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 
 }
