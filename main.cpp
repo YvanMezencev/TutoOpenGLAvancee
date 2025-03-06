0f68e655a401ed3763da49e197b5ac0187b2d7f3
Rgautier35 <160230909+Rgautier35@users.noreply.github.com>
2025-03-06 10:14:53 +0100

    !lkjnbdsqfmkjf

--- main.cpp
+++ main.cpp
@@ -41,6 +41,12 @@ int main()
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
 	while (!glfwWindowShouldClose(window))
 	{
 		//Input
